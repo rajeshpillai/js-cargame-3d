4da407359f4251a1dfb0f74e81d1a33fd3b29b88
Rajesh Pillai <pillai.rajesh@gmail.com>
2022-05-21 05:04:26 +0000

    Setting up textures

--- car.js
+++ car.js
@@ -31,10 +31,28 @@ function Car() {
   main.position.z = 12;
   car.add(main);
 
+  const carFrontTexture = getCarFrontTexture();
+  carFrontTexture.center = new THREE.Vector2(0.5, 0.5);
+  carFrontTexture.rotation = Math.PI / 2;
+  
+  const carBackTexture = getCarFrontTexture();
+  carBackTexture.center = new THREE.Vector2(0.5, 0.5);
+  carBackTexture.rotation = -Math.PI / 2;
+  
+  const carRightSideTexture = getCarSideTexture();
+  
+  const carLeftSideTexture = getCarSideTexture();
+  carLeftSideTexture.flipY = false;
+
   const cabin = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(33,24,12),
-    new THREE.MeshLambertMaterial({color: 0xffffff})
-  );
+    new THREE.BoxBufferGeometry(33,24,12),[
+    new THREE.MeshLambertMaterial({map: carFrontTexture}),
+    new THREE.MeshLambertMaterial({map: carBackTexture}),
+    new THREE.MeshLambertMaterial({map: carLeftSideTexture}),
+    new THREE.MeshLambertMaterial({map: carRightSideTexture}),
+    new THREE.MeshLambertMaterial({color: 0xffffff}), // top
+    new THREE.MeshLambertMaterial({color: 0xffffff}), // bottom
+  ]);
 
   cabin.position.x = -6;
   cabin.position.z = 25.5;
@@ -54,4 +72,35 @@ function Wheel() {
 
 function pickRandom(array) {
   return array[Math.floor(Math.random() * array.length)];
+}
+
+function getCarFrontTexture() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 64;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = "#ffffff";
+  context.fillRect(0, 0, 64, 32);
+
+  context.fillStyle = "#666666";
+  context.fillRect(8, 8, 48, 24);
+
+  return new THREE.CanvasTexture(canvas);
+}
+
+function getCarSideTexture() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 128;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = "#ffffff";
+  context.fillRect(0, 0, 128, 32);
+
+  context.fillStyle = "#666666";
+  context.fillRect(10, 8, 38, 24);
+  context.fillRect(58, 8, 60, 24);
+
+  return new THREE.CanvasTexture(canvas);
 }
