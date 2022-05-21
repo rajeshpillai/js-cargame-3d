e1f85acc6fdfe29bb95137ed4ce6e0ee0a3f0c70
Rajesh Pillai <pillai.rajesh@gmail.com>
2022-05-21 04:51:45 +0000

    Initial commit

---
+++ car.js
@@ -0,0 +1,45 @@
+function Car() {
+  const car = new THREE.Group();
+
+  const backWheel = Wheel();
+
+  car.add(backWheel);
+  backWheel.position.z = 6;
+  backWheel.position.x = -18;
+
+
+  // Frontwheel
+  const frontWheel = Wheel();
+  frontWheel.position.z = 6;
+  frontWheel.position.x = 18;
+  
+  car.add(frontWheel);
+
+  const main = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(60,30,15),
+    new THREE.MeshLambertMaterial({color: 0xa52523})
+  );
+
+  main.position.z = 12;
+  car.add(main);
+
+  const cabin = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(33,24,12),
+    new THREE.MeshLambertMaterial({color: 0xffffff})
+  );
+
+  cabin.position.x = -6;
+  cabin.position.z = 25.5;
+  car.add(cabin);
+  
+  return car;
+}
+
+function Wheel() {
+   const wheel = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(12, 33, 12),
+    new THREE.MeshLambertMaterial({color: 0x333333})
+  );
+  wheel.position.z = 6;
+  return wheel;
+}
